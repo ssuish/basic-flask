d20ab80daa586d89154b347ef28293309d07ec39
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-21 21:58:05 +0800

    add gitignore, new app

--- src/app.py
+++ src/app.py
@@ -1,1 +1,15 @@
-# Basic flask
+# Basic flask web app
+from flask import Flask, render_template, request, redirect, url_for
+
+# Create a flask app
+app = Flask(__name__)
+
+# Define the route for the default web page
+@app.route('/')
+def home():
+    return "Hello World!"
+# Define the route for the about page
+@app.route('/about')
+def about():
+    return "About page"
+
