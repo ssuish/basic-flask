9b342614ce6eb0e3ccc453288aab671d14635c4b
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-24 21:50:35 +0800

    TEMP: inheritance

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def home():
 def about():
     return render_template('about.html', posts=src.post.post_list, title="About Us")
 
+@app.route('/inheritance')
+def inherit():
+    return render_template('inheritance.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
